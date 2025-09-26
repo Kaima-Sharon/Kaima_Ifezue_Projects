e5a30d14db74ef608341b1c7ad6454960ba4c1b7
Ifezue Chikaima <116591243+Kaima-Sharon@users.noreply.github.com>
2025-09-26 13:39:10 -0400

    Update firebase.js

--- ChapterChat/client/src/firebase.js
+++ ChapterChat/client/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUmPh7f-KEdbvIMdT4ego_yt5MOUUmqP4",
-  authDomain: "chapterchat-498eb.firebaseapp.com",
-  projectId: "chapterchat-498eb",
-  storageBucket: "chapterchat-498eb.firebasestorage.app",
-  messagingSenderId: "1005713428425",
-  appId: "1:1005713428425:web:afdb5588523349de582218",
-  measurementId: "G-SERH3M1Y8Z"
+  apiKey: "####################",
+  authDomain: "#######################",
+  projectId: "############",
+  storageBucket: "##################",
+  messagingSenderId: "######################",
+  appId: "##############################",
+  measurementId: "################"
 };
 
 // Initialize Firebase
